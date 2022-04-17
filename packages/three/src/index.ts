8f17472dca0e312e87e38cd2623d045452307b0e
Sergey Bondar <abobopw@gmail.com>
2022-04-17 14:14:30 +0300

    feat: update second and third packages

--- packages/three/src/index.ts
+++ packages/three/src/index.ts
@@ -1,4 +1,4 @@
-const version = "1.0.7";
+const version = "1.0.8";
 
 export const threeFn = () => {
   return 'this is from package three ' + version
--- packages/two/src/index.ts
+++ packages/two/src/index.ts
@@ -1,4 +1,4 @@
-const version = "1.0.4";
+const version = "1.0.5";
 
 export const twoFn = () => {
   return 'this is from package two ' + version
